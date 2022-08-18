f0514c2a605201abed8349fe25d58ea53024ea17
apark0819 <apark0819@gmail.com>
2022-08-17 23:12:59 -0700

    functions without css

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,7 @@ var finish =document.querySelector("#fin");
 var backBtn =document.querySelector("#back_btn");
 var clearBtn=document.querySelector("#clear_btn");
 
+// Questions
 var testQuestions = [
     {
         question: "Questions 1 : What does HTML stand for?",
@@ -54,3 +55,175 @@ var testQuestions = [
     },
    
 ];
+
+var timeLeft = document.getElementById("timer");
+
+var secondsLeft = 60;
+var questionNumber = 0;
+var totalScore = 0;
+var questionCount = 1;
+
+function countdown() {
+        
+    var timerInterval = setInterval(function () {
+
+      secondsLeft--;
+      timeLeft.textContent = "Time left: " + secondsLeft + " s";
+
+        if (secondsLeft <= 0){
+            clearInterval(timerInterval);
+            timeLeft.textContent = "Time is up!"; 
+            finish.textContent = "Time is up!";
+            gameOver();
+
+        } else  if(questionCount >= testQuestions.length +1) {
+            clearInterval(timerInterval);
+            gameOver();
+            } 
+    }, 1000);
+}
+
+function startQuiz () {
+    introPage.style.display = "none";
+    questionPage.style.display = "block";
+    questionNumber = 0
+    countdown();
+    showQuestion(questionNumber);
+  
+}
+
+function showQuestion (a) {
+    askQuestion.textContent = testQuestions[a].question;
+    answerBtn1.textContent = testQuestions[a].choices[0];
+    answerBtn2.textContent = testQuestions[a].choices[1];
+    answerBtn3.textContent = testQuestions[a].choices[2];
+    answerBtn4.textContent = testQuestions[a].choices[3];
+    questionNumber = a;
+}
+
+function checkAnswer(event) {
+    event.preventDefault();
+    checkLine.style.display = "block";
+    setTimeout(function () {
+        checkLine.style.display = 'none';
+    }, 1000);
+
+    if (testQuestions[questionNumber].answer == event.target.value) {
+        checkLine.textContent = "Correct!"; 
+        totalScore = totalScore + 1;
+
+    } else {
+        secondsLeft = secondsLeft - 10;
+        checkLine.textContent = "Wrong! The correct answer is " + testQuestions[questionNumber].answer + " .";
+    }
+    if (questionNumber < testQuestions.length -1 ) {
+        showQuestion(questionNumber +1);
+    } else {
+    gameOver();
+}
+questionCount++;
+}
+
+function gameOver() {
+
+    questionPage.style.display = "none";
+    scoreBoard.style.display = "block";
+    console.log(scoreBoard);
+    // show final score
+    finalScore.textContent = "Your final score is :" + totalScore ;
+    // clearInterval(timerInterval);  
+    timeLeft.style.display = "none"; 
+};
+
+function getScore () {
+    var currentList =localStorage.getItem("ScoreList");
+    if (currentList !== null ){
+        freshList = JSON.parse(currentList);
+        return freshList;
+    } else {
+        freshList = [];
+    }
+    return freshList;
+};
+
+function renderScore () {
+    scoreRecord.innerHTML = "";
+    scoreRecord.style.display ="block";
+    var highScores = sort();   
+    var topFive = highScores.slice(0,5);
+    for (var i = 0; i < topFive.length; i++) {
+        var item = topFive[i];
+    var li = document.createElement("li");
+    li.textContent = item.user + " - " + item.score;
+    li.setAttribute("data-index", i);
+    scoreRecord.appendChild(li);
+    }
+};
+
+function sort () {
+    var unsortedList = getScore();
+    if (getScore == null ){
+        return;
+    } else{
+    unsortedList.sort(function(a,b){
+        return b.score - a.score;
+    })
+    return unsortedList;
+}};
+
+function addItem (n) {
+    var addedList = getScore();
+    addedList.push(n);
+    localStorage.setItem("ScoreList", JSON.stringify(addedList));
+};
+
+function saveScore () {
+    var scoreItem ={
+        user: userInitial.value,
+        score: totalScore
+    }
+    addItem(scoreItem);
+    renderScore();
+}
+
+
+startBtn.addEventListener("click", startQuiz);
+
+reactButtons.forEach(function(click){
+
+    click.addEventListener("click", checkAnswer);
+});
+submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    scoreBoard.style.display = "none";
+    introPage.style.display = "none";
+    highScorePage.style.display = "block";
+    questionPage.style.display ="none";
+    saveScore();
+});
+
+//score 
+scoreCheck.addEventListener("click", function(event) {
+    event.preventDefault();
+    scoreBoard.style.display = "none";
+    introPage.style.display = "none";
+    highScorePage.style.display = "block";
+    questionPage.style.display ="none";
+    renderScore();
+});
+
+//main page back
+backBtn.addEventListener("click",function(event){
+        event.preventDefault();
+        scoreBoard.style.display = "none";
+        introPage.style.display = "block";
+        highScorePage.style.display = "none";
+        questionPage.style.display ="none";
+        location.reload();
+});
+
+clearBtn.addEventListener("click",function(event) {
+    event.preventDefault();
+    localStorage.clear();
+    renderScore();
+});
