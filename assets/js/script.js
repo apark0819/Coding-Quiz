3beb0797029dfaf2244f79c8a17796ab3173b908
apark0819 <apark0819@gmail.com>
2022-08-17 22:20:03 -0700

    js variables

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+//Variables
+var welcome = document.querySelector("#introduction");
+var startBtn = document.querySelector("#start_button");
+var introPage =document.querySelector("#intro_page");
+
+var questionPage = document.querySelector("#questions_page");
+var askQuestion = document.querySelector("#questions");
+
+var reactButtons = document.querySelectorAll(".choices");
+var answerBtn1 = document.querySelector("#answer_btn1");
+var answerBtn2 = document.querySelector("#answer_btn2");
+var answerBtn3 = document.querySelector("#answer_btn3");
+var answerBtn4 = document.querySelector("#answer_btn4");
+
+var checkLine = document.querySelector("#question_checker");
+var scoreBoard = document.querySelector("#results_page");
+var finalScore = document.querySelector("#score");
+var userInitial =document.querySelector("#user-name");
+
+var submitBtn =document.querySelector("#submit_btn");
+var highScorePage =document.querySelector("#highscore_page");
+var scoreRecord =document.querySelector("#score_record");
+var scoreCheck =document.querySelector("#score_check");
+var finish =document.querySelector("#finish");
+
+var backBtn =document.querySelector("#back_btn");
+var clearBtn=document.querySelector("#clear_btn");
